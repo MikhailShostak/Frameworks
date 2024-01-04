c5cdf00dc2e502c8d5eb898f2951602391b47f2f
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2024-01-04 17:23:32 +0300

    ImGuiUtils

--- UI/Include/ImGuiUtils.hpp
+++ UI/Include/ImGuiUtils.hpp
@@ -3,7 +3,7 @@
 namespace ImGui
 {
 
-inline void Image(Graphics::Texture &Texture, const Float2 &Size)
+inline void Image(Graphics::Texture &Texture, const Float2 &Size = { 0, 0 }, const Float4& UV = {0, 0, 1, 1}, const Float4& TintColor = {1, 1, 1, 1}, const Float4& BorderColor = {0, 0, 0, 0})
 {
     ImTextureID handle = *((ImTextureID*)&Texture.Data);
     if (handle == 0)
@@ -12,7 +12,19 @@ inline void Image(Graphics::Texture &Texture, const Float2 &Size)
         handle = *((ImTextureID*)&Texture.Data);
     }
 
-    ImGui::Image(handle, ImVec2(Size[0], Size[1]));
+    ImGui::Image(
+        handle,
+        { Size[0] != 0.0f ? Size[0] : Texture.Size[0], Size[1] != 0.0f ? Size[1] : Texture.Size[1] },
+        { UV[0], UV[1] },
+        { UV[2], UV[3] },
+        { TintColor[0], TintColor[1], TintColor[2], TintColor[3] },
+        { BorderColor[0], BorderColor[1], BorderColor[2], BorderColor[3] }
+    );
+}
+
+inline void Image(const SharedReference<Graphics::Texture> &Texture, const Float2 &Size = { 0, 0 }, const Float4& UV = {0, 0, 1, 1}, const Float4& TintColor = {1, 1, 1, 1}, const Float4& BorderColor = {0, 0, 0, 0})
+{
+    Image(*Texture, Size, UV, TintColor, BorderColor);
 }
 
 }
